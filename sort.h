63128a41141257ca17495a52bbb01d18e4011f09
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-07-22 15:01:11 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,8 +1,23 @@
-#ifndef _SORTING_A_
-#define _SORTING_A_
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -10,9 +25,6 @@
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
- *
- * Description:
- *      This struct defines a node for a doubly linked list of integers.
  */
 typedef struct listint_s
 {
@@ -21,20 +33,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-listint_t *create_listint(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void heap_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
-void swap(int *a, int *b);
-void heapify(int *array, int index, size_t size);
+
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-int partition(int *array, int lower, int higher, int size);
-void set_pivot(int array[], int low, int high, int size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-
-#endif /* _SORTING_A_ */
+#endif /* SORT_H */
