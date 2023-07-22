77ad9010be5c92c92bab89f1e95c5627168f87a9
SarahEmmy <bawahsarah@gmail.com>
2023-07-22 06:47:30 -0700

    updates

--- sort.h
+++ sort.h
@@ -34,6 +34,7 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, int lower, int higher, int size);
 void set_pivot(int array[], int low, int high, int size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 
 #endif /* _SORTING_A_ */
