590d25984272e3c8a450df545bcc90cc06b1e8db
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-07-22 11:42:49 +0100

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,10 +1,16 @@
 #include "sort.h"
 
 /**
- * shell_sort - sorts an array of integers in ascending order
+ * shell_sort - Sorts an array of integers in ascending order
  * @array: Array of integers
  * @size: Size of the array
- * Return - void
+ * 
+ * Description:
+ *      This function implements the Shell sort algorithm to sort an array
+ *      of integers in ascending order.
+ *      
+ * Return:
+ *      Void - The sorted array is modified in place.
  */
 void shell_sort(int *array, size_t size)
 {
@@ -12,8 +18,10 @@ void shell_sort(int *array, size_t size)
 	int tmp; /*tmporal variable for swap*/
 
 	if (!array || size < 2)
+		/* If array is NULL or size is less than 2, there is nothing to sort */
 		return;
 
+	 /* Initialize the gap (distance between elements to be compared) */
 	while (space < size / 3)
 	{
 		space = space * 3 + 1;
@@ -21,9 +29,11 @@ void shell_sort(int *array, size_t size)
 
 	while (space > 0)
 	{
+		* Perform insertion sort with the current gap */
 		for (i = space; i < size; i++)
 		{
 			tmp = array[i];
+			/* Shift elements to create the right gap for insertion */
 			for (i = space; i < size; i += 1)
 			{
 				tmp = array[i];
